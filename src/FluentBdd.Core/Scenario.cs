4133b1bb2cbfd9b3244b2cad7714ac942222a5fd
Zack Schrag <zackschrag@gmail.com>
2022-05-10 20:05:27 -0600

    Move ScenarioStep to own file

--- src/FluentBdd.Core/Scenario.cs
+++ src/FluentBdd.Core/Scenario.cs
@@ -2,14 +2,6 @@
 
 namespace FluentBdd.Core
 {
-    public class ScenarioStep<T>
-    {
-        public string? StepType { get; set; }
-        public string? StepDescription { get; set; }
-        public Action<T>? Action { get; set; }
-        public Func<T, Task>? AsyncFunc { get; set; }
-    }
-
     public class Scenario<T>
     {
         public Feature Feature { get; }
---
+++ src/FluentBdd.Core/ScenarioStep.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentBdd.Core;
+
+public class ScenarioStep<T>
+{
+    public string? StepType { get; set; }
+    public string? StepDescription { get; set; }
+    public Action<T>? Action { get; set; }
+    public Func<T, Task>? AsyncFunc { get; set; }
+}
